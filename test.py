887ba152117975744ebbe9921a9b9c9795575eb9
Pascal Henrich <pascalh.1505@gmail.com>
2021-02-01 22:35:42 +0100

    test push

---
+++ test.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+pygame.display.set_mode((800, 600))
+# Schleife Hauptprogramm
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
